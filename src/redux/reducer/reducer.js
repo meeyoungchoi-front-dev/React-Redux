32a0bca230efe02885e624841bdd10bae8688b94
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-24 11:15:08 +0900

    redux 예제 - 증가, 감소, 로그인

--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,10 +1,16 @@
 let initialState = {
-    count: 0
+    count: 0,
+    id: "",
+    password:""
 };
 function reducer(state=initialState,action) {
     console.log("action:", action);
     if (action.type === "INCREMENT") {
-        return {...state,count: state.count + 1};
+        return {...state,count: state.count + action.payload.num};
+    } else if (action.type === "LOGIN") {
+        return {...state,id: action.payload.id, password: action.payload.password }
+    } else if (action.type === "DECRECEMENT") {
+        return {...state, count: state.count - action.payload.num};
     }
 
     return {...state};
